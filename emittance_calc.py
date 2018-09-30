b5a52feaab31d46c88941e9408fd6df6a60d1907
nicole <nneveu@hawk.iit.edu>
2018-09-29 21:06:17 -0500

    ?

---
+++ emittance_calc.py
@@ -0,0 +1,34 @@
+import numpy as np
+import h5py 
+import matplotlib.pyplot as plt
+
+#Load the h5 dataset 
+
+
+
+#Find out what data is in the file
+
+
+#Make a scatter plot of 2 variables in the data set
+
+
+#Make histograms of the data
+
+
+
+#Calculate the x, and px mean
+
+
+#Calculate the correleation between x and px
+
+
+
+
+#Calculate the emittance
+
+
+
+#Calculate the location of the the center of the beam.
+
+
+
